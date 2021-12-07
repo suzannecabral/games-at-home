959b17e6cfedb7efbbd08d4d684d25c480fd36b1
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-12-07 15:41:57 -0800

    boilerplate & test formatting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="panel">
+				<p>Enter your steam url:</p>
+				<p class="caption">(Must be set to public)</p>
+				<input />
+				<button class="secondary-button">Save</button>
+			</div>
+			<h1>Pick a game:</h1>
+			<div className="panel half">
+				<p>Your Result</p>
+			</div>
+			<button>Another!</button>
+			<button className="secondary-button">Store Page</button>
+
+			<h2>Your games library:</h2>
+			<div className="panel">
+				<ul>
+					<li>thing</li>
+					<li>thing</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
